bc38ad676daf36b6d5e8f89b52da7e2da39823b4
gegorov <gegorov@yahoo.com>
2020-07-01 01:32:54 +0300

    Optimize modal rendering

--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import classes from './Modal.module.css';
@@ -11,23 +11,33 @@ const propTypes = {
   show: PropTypes.bool.isRequired,
 };
 
-const Modal = (props) => {
-  const { children, modalClosed, show } = props;
-  return (
-    <Aux>
-      <Backdrop show={show} clicked={modalClosed} />
-      <div
-        className={classes.Modal}
-        style={{
-          transform: show ? 'translateY(0)' : 'translateY(-100vh',
-          opacity: show ? '1' : '0',
-        }}
-      >
-        {children}
-      </div>
-    </Aux>
-  );
-};
+class Modal extends Component {
+  shouldComponentUpdate(nextProps) {
+    const { show } = this.props;
+    if (nextProps.show !== show) {
+      return true;
+    }
+    return false;
+  }
+
+  render() {
+    const { children, modalClosed, show } = this.props;
+    return (
+      <Aux>
+        <Backdrop show={show} clicked={modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: show ? 'translateY(0)' : 'translateY(-100vh',
+            opacity: show ? '1' : '0',
+          }}
+        >
+          {children}
+        </div>
+      </Aux>
+    );
+  }
+}
 
 Modal.propTypes = propTypes;
 
