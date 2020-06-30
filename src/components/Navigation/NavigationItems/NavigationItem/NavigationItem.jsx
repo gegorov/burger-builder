9712d61fb1cea7603fb0f57880d587fc7c182aad
gegorov <gegorov@yahoo.com>
2020-07-01 01:14:37 +0300

    Adjust eslint

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.jsx
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import classes from './NavigationItem.module.css';
 
 const propTypes = {
-  active: PropTypes.string.isRequired,
+  active: PropTypes.bool.isRequired,
   children: PropTypes.string.isRequired,
   link: PropTypes.string.isRequired,
 };
